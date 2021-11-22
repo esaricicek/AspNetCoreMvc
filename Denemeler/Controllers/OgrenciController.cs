621809016cae87278ed1e8d22a6e87ce41c206be
Emrah SARICICEK <emrah.saricicek@securist.com.tr>
2021-11-22 10:25:01 +0300

    Last commit

---
+++ Denemeler/Controllers/OgrenciController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Denemeler.Controllers
+{
+    public class OgrenciController : Controller
+    {
+
+        public IActionResult Listele()
+        {
+            return View(Models.OgrenciVeri.Ogrenciler);
+        }
+
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Yeni(Models.Ogrenci yeniOgrenci)
+        {
+            Models.OgrenciVeri.Ogrenciler.Add(yeniOgrenci);
+            return RedirectToAction("Listele");            
+        }
+
+        public IActionResult Guncelle(int id)
+        {
+            //  Linq
+            //select top 1 * from Ogrenciler where Id = ogrenciId
+            var r = Models.OgrenciVeri.Ogrenciler.FirstOrDefault(x => x.Id == id);
+            return View(r);
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle(Models.Ogrenci ogrenci)
+        {
+            var c = Models.OgrenciVeri.Ogrenciler.FirstOrDefault(x => x.Id == ogrenci.Id);
+
+            var r = Models.OgrenciVeri.Ogrenciler.FirstOrDefault(x => x.Id == ogrenci.Id);
+            r.Ad = ogrenci.Ad;
+            r.Soyad = ogrenci.Soyad;
+            r.BolumAd = ogrenci.BolumAd;
+            r.TCKimlikNo = ogrenci.TCKimlikNo;
+            r.GirisYil = ogrenci.GirisYil;
+            
+            Models.OgrenciVeri.Ogrenciler.Remove(c);
+
+            Models.OgrenciVeri.Ogrenciler.Add(r);
+
+            return RedirectToAction("Listele");
+        }
+
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ Denemeler/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Denemeler.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Listele()
+        {
+            return View(Models.OgrenciVeri.Ogrenciler);
+        }
+
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+        
+
+        [HttpPost]
+        public IActionResult Yeni(Models.Ogrenci ogrenci)
+        {
+            Models.OgrenciVeri.Ogrenciler.Add(ogrenci);
+            return RedirectToAction("Listele");
+        }
+
+    }
+}
